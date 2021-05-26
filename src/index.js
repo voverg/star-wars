dc6b78bf12e2e0aca4a7f4bb00a3265e65eb5609
voverg <voverg81@mail.ru>
2021-05-26 14:50:15 +0300

    Create SwapiService class for working with api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const url = 'https://swapi.dev/api/people/1/';
+class SwapiService {
+  _apiBase = 'https://swapi.dev/api';
 
-class swapiService {
   async getResourse(url) {
-    const response = await fetch(url);
+    const response = await fetch(`${this._apiBase}${url}`);
     if (!response.ok) {
       throw new Error(`Could not fetch ${url}, received ${response.status}`)
     }
@@ -11,4 +11,36 @@ class swapiService {
     return body;
   }
 
+  async getAllPeople() {
+    const res = await this.getResourse(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResourse(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResourse(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResourse(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResourse(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResourse(`/starships/${id}`);
+  }
 }
+
+const swapi = new SwapiService();
+
+swapi.getPlanet(3).then((people) => {
+  console.log(people.name);
+});
